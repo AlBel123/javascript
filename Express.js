0cbe90611a602b39e01baf5839d5529fa0479e1f
AlBel123 <133576560+AlBel123@users.noreply.github.com>
2023-07-07 02:04:08 +0100

    Update Express.js

--- Express.js
+++ Express.js
@@ -3,9 +3,28 @@
 const express=require("express");
 const app=express();
 
-app.get("/", function(req, res){
-    response.send("<h1>Hello World</h1>");
+=== Routs in express.js==================================================================================
+  
+app.get('/', function(req, res){
+res.send("<h1>This is my first server</h1> <h2> welcome to BackUp codding</h2>")
 });
+//http://localhost:3000
+
+app.get('/contact', function (req,res){
+    res.send('<h2>Contact me at email@domain</h2>')
+})
+//http://localhost:3000/contact
+
+app.get('/about', function(req,res){
+    res.send('<h3>Hi there. My name is Aleksander and I am a programnpm instamer</h3>')
+})
+//http://localhost:3000/about
+
+app.get('/hobbies', function(req, res){
+    res.send('I like beer and buffalo wings')
+})
+//http://localhost:3000/hobbies
+
 
 app.listen(3000, function(){
     console.log("Server started at port 3000");
